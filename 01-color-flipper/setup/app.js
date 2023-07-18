caeeefc8f7de26e88311c320948ea38077d4f9b5
Bibin <bibin@uniekesoftware.com>
2023-07-18 18:39:34 +0100

    01 update

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,10 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.getElementById(".color");
+
+btn.addEventListener('click',function(){
+
+    
+console.log(document.body)
+
+})
